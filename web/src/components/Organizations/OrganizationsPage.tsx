64b88ec30de3ec902b4b454df98ff391125ed012
Artyom <55352909+arsnatenkov@users.noreply.github.com>
2023-04-12 22:07:45 +0300

    feat: add tabs on pages with fix

--- web/src/components/Organizations/OrganizationsPage.tsx
+++ web/src/components/Organizations/OrganizationsPage.tsx
@@ -5,25 +5,15 @@ import {FC, useEffect, useState} from 'react'
 import {useLanguage} from "context/Translation";
 import {organizationService} from 'services/organizations'
 import {scrollTopPage} from 'utils'
-import {Box, Grid, Tab} from '@mui/material'
+import {Box, Grid} from '@mui/material'
 import {OrganizationCard} from 'components/common'
 import {observer} from 'mobx-react-lite'
 import Paging from 'components/common/Paging'
-import {productService} from "../../services/product";
-import Container from "../common/Container/Container";
-import {TabContext, TabList, TabPanel} from "@mui/lab";
-import {ProductCard} from "../common/Cards/Products";
-import useFilterAndListContainerStyle from "../common/Container/FilterAndListContainer/FilterAndListContainer.styles";
+import {TabIO} from "../common/Tabs";
 
 const OrganizationsPage: FC = () => {
     const {language} = useLanguage();
-    const {classes} = useFilterAndListContainerStyle();
     const {organizations, filterFields, paging, loading} = organizationService;
-    const [value, setValue] = useState('1')
-
-    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-        setValue(newValue)
-    }
     useEffect(() => {
         organizationService.fetchPagingOrganizations(0, language);
         organizationService.fetchFilterElements(language)
@@ -43,49 +33,38 @@ const OrganizationsPage: FC = () => {
                 <Header/>
                 <Carousel/>
                 <Breadcrumb/>
-                <Container>
-                    <Grid container>
-                        <Grid item width={'25%'} className={classes.root}>
-                            {
-                                <Filter onFind={handleFindClick} filterElements={filterFields}
-                                        filterParams={organizationService.getFilterValues()}/>
-                            }
-                        </Grid>
-                        <Grid item width={'75%'} className={classes.root}>
-                            {
-                                <Container>
-                                    <TabContext value={value}>
-                                        <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
-                                            <TabList aria-label='Tabs example' onChange={handleChange}>
-                                                <Tab label='Организации' value='1'/>
-                                                <Tab label='Анализ данных' value='2'/>
-                                            </TabList>
-                                        </Box>
-                                        <TabPanel value='1'>
-                                            <Grid container spacing={2}>
-                                                {organizations && organizations.length ? (
-                                                    organizations.map((org, index) => {
-                                                        return (
-                                                            <Grid item key={index} width={'100%'}>
-                                                                <OrganizationCard item={org.item}/>
-                                                            </Grid>
-                                                        )
-                                                    })
-                                                ) : (
-                                                    <></>
-                                                )}
-                                            </Grid>
-                                            <Paging paging={paging} onChange={handlePageChange}/>
+                <FilterAndListContainer
+                    filter={
+                        <Filter onFind={handleFindClick} filterElements={filterFields}
+                                filterParams={organizationService.getFilterValues()}/>
+                    }
+                    list={
 
-                                        </TabPanel>
-                                        <TabPanel value='2'>
-                                        </TabPanel>
-                                    </TabContext>
-                                </Container>
-                            }
-                        </Grid>
-                    </Grid>
-                </Container>
+
+                        <TabIO
+                            list={
+                                <Box>
+                                    <Box>
+                                        <Grid container spacing={2}>
+                                            {organizations && organizations.length ? (
+                                                organizations.map((org, index) => {
+                                                    return (
+                                                        <Grid item key={index} width={'100%'}>
+                                                            <OrganizationCard item={org.item}/>
+                                                        </Grid>
+                                                    )
+                                                })
+                                            ) : (
+                                                <></>
+                                            )}
+                                        </Grid>
+                                        <Paging paging={paging} onChange={handlePageChange}/>
+                                    </Box>
+                                </Box>
+                            } name={"Организации"}/>
+
+                    }
+                />
                 <Footer/>
             </>
         )}
--- web/src/components/ResearchCenters/ResearchCentersPage.tsx
+++ web/src/components/ResearchCenters/ResearchCentersPage.tsx
@@ -1,4 +1,4 @@
-import {Box, Grid, Tab} from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import { Breadcrumb, Filter, FilterAndListContainer, Footer, Spinner } from 'components/common';
 import { ResearchCenterCard } from 'components/common/Cards/ResearchCenters';
 import { Carousel } from 'components/common/Carousel'
@@ -6,24 +6,14 @@ import Header from 'components/common/Header'
 import Paging from 'components/common/Paging';
 import { useLanguage } from 'context/Translation';
 import { observer } from 'mobx-react-lite';
-import {FC, useEffect, useState} from 'react'
+import { FC, useEffect } from 'react'
 import { researchCenterService } from 'services/researchCenter';
 import { scrollTopPage } from 'utils';
-import {projectService} from "../../services/projects";
-import Container from "../common/Container/Container";
-import {TabContext, TabList, TabPanel} from "@mui/lab";
-import {ProjectCard} from "../common/Cards/Projects";
-import useFilterAndListContainerStyle from "../common/Container/FilterAndListContainer/FilterAndListContainer.styles";
+import {TabIO} from "../common/Tabs";
 
 const ResearchCentersPage: FC = () => {
     const { language } = useLanguage();
-    const {classes} = useFilterAndListContainerStyle();
     const { ResearchCenters, paging, filterFields, loading } = researchCenterService;
-    const [value, setValue] = useState('1')
-
-    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-        setValue(newValue)
-    }
     useEffect(() => {
         researchCenterService.fetchPagingResearchCenters(0, language);
         researchCenterService.fetchFilterElements(language)
@@ -46,48 +36,35 @@ const ResearchCentersPage: FC = () => {
                     <Header />
                     <Carousel />
                     <Breadcrumb />
-                    <Container>
-                        <Grid container>
-                            <Grid item width={'25%'} className={classes.root}>
-                                {
-                                    <Filter onFind={handleFindClick} filterElements={filterFields} filterParams={researchCenterService.getFilterValues()}/>
-                                }
-                            </Grid>
-                            <Grid item width={'75%'} className={classes.root}>
-                                {
-                                    <Container>
-                                        <TabContext value={value}>
-                                            <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
-                                                <TabList aria-label='Tabs example' onChange={handleChange}>
-                                                    <Tab label='Исследовательские центры' value='1'/>
-                                                    <Tab label='Анализ данных' value='2'/>
-                                                </TabList>
-                                            </Box>
-                                            <TabPanel value='1'>
-                                                <Grid container spacing={2}>
-                                                    {ResearchCenters && ResearchCenters.length ? (
-                                                        ResearchCenters.map((researchCenter, index) => {
-                                                            return (
-                                                                <Grid item key={index} width={'100%'}>
-                                                                    <ResearchCenterCard item={researchCenter.item} />
-                                                                </Grid>
-                                                            )
-                                                        })
-                                                    ) : (
-                                                        <></>
-                                                    )}
-                                                </Grid>
-                                                <Paging paging={paging} onChange={handlePageChange}/>
+                    <FilterAndListContainer
+                        filter={
+                            <Filter onFind={handleFindClick} filterElements={filterFields} filterParams={researchCenterService.getFilterValues()}/>
+                        }
+                        list={
+                        <TabIO
+                            list={
+                                <Box >
+                                    <Box>
+                                        <Grid container spacing={2}>
+                                            {ResearchCenters && ResearchCenters.length ? (
+                                                ResearchCenters.map((researchCenter, index) => {
+                                                    return (
+                                                        <Grid item key={index} width={'100%'}>
+                                                            <ResearchCenterCard item={researchCenter.item} />
+                                                        </Grid>
+                                                    )
+                                                })
+                                            ) : (
+                                                <></>
+                                            )}
+                                        </Grid>
+                                        <Paging paging={paging} onChange={handlePageChange} />
+                                    </Box>
+                                </Box>
+                            } name={"Исследовательские центры"}/>
 
-                                            </TabPanel>
-                                            <TabPanel value='2'>
-                                            </TabPanel>
-                                        </TabContext>
-                                    </Container>
-                                }
-                            </Grid>
-                        </Grid>
-                    </Container>
+                        }
+                    />
                     <Footer />
                 </>
             )
